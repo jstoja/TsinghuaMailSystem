b5415be91a17aeb9e21361b7a3a9c7316dc544c0
Julien Bordellier <julienbordellier@gmail.com>
2014-06-07 12:34:08 +0800

    Angular interface prototype

---
+++ web/js/main.js
@@ -0,0 +1,14 @@
+(function(){
+	var app = angular.module("thumail", []);
+
+	app.controller("MailboxController", ['$http', function($http){
+		var mailbox = this;
+		mailbox.mails = [];
+		mailbox.currentMail = {};
+
+		$http.get('/mini-last.json').success(function(data){
+			mailbox.mails = data;
+			mailbox.currentMail = data[0];
+		});
+	}]);
+})();
